78a703526eca95fa12e565369b64a939ad143479
Kyle Heidorn <kyle841@revature.net>
2024-07-18 17:21:03 -0400

    post model

--- Revature-P1/src/main/java/com/revature/p1/Models/Post.java
+++ Revature-P1/src/main/java/com/revature/p1/Models/Post.java
@@ -1,28 +1,61 @@
 package com.revature.p1.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
-@Table(name = "Posts")
+@Table(name = "posts")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "pID")
-    private Integer pID;
 
-    @Column(name = "content")
-    String content;
+    private Integer postId;
 
-    @Column(name = "uID")
-    Integer uID;
 
-    public Integer getpID() {
-        return pID;
+    private String content;
+
+
+    private int like;
+
+
+    private int shares;
+
+//    @Column(name = "timestamp")
+//    private Date timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @OneToMany(mappedBy = "post")
+    @JsonManagedReference
+    List<Comment> comments;
+
+    @OneToMany(mappedBy = "post")
+    @JsonManagedReference
+    List<Likes> likes;
+
+    public Post(Integer postId, String content, int like, int shares, User user) {
+        this.postId = postId;
+        this.content = content;
+        this.like = like;
+        this.shares = shares;
+        //this.timestamp = timestamp;
+        this.user = user;
     }
 
-    public void setpID(Integer pID) {
-        this.pID = pID;
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
     }
 
     public String getContent() {
@@ -33,11 +66,36 @@ public class Post {
         this.content = content;
     }
 
-    public Integer getuID() {
-        return uID;
+    public int getLike() {
+        return like;
     }
 
-    public void setuID(Integer uID) {
-        this.uID = uID;
+    public void setLike(int like) {
+        this.like = like;
     }
+
+    public int getShares() {
+        return shares;
+    }
+
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+
+//    public Date getTimestamp() {
+//        return timestamp;
+//    }
+
+//    public void setTimestamp(Date timestamp) {
+//        this.timestamp = timestamp;
+//    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
