a9268806c92b9d6924986de9e7bbe2e2176ebb6e
Brittany Moliver <britt.moliver@gmail.com>
2024-07-19 15:44:56 -0400

    edits on new branch Britt

--- src/main/java/com/revature/p1/Repositories/FollowRepository.java
+++ src/main/java/com/revature/p1/Repositories/FollowRepository.java
@@ -8,5 +8,6 @@ import com.revature.p1.Models.*;
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
     // https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
-
+    List<Follow> findByFollowerId(Integer followerId);      // list of ppl the user is following, where the user is the "follower"
+    List<Follow> findByFollowingId(Integer followingId);    // list of the user's followers, where the user is the "following"
 }
--- src/main/java/com/revature/p1/Repositories/LikeRepository.java
+++ src/main/java/com/revature/p1/Repositories/LikeRepository.java
@@ -8,5 +8,6 @@ import com.revature.p1.Models.*;
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
     // https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
+    List<Like> findByPostId(Int postId);
 
 }
