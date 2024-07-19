18f145f2c5f5ac0306e2b867dd3eff312714521b
BlueWhaleEnthusiast <aaks.hemdev@gmail.com>
2024-07-19 17:27:56 -0400

    new branch

--- src/main/java/com/revature/p1/Repositories/LikeRepository.java
+++ src/main/java/com/revature/p1/Repositories/LikeRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.revature.p1.Models.*;
 
+import java.util.List;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
     // https://docs.spring.io/spring-data/jpa/docs/1.6.0.RELEASE/reference/html/jpa.repositories.html
-    List<Like> findByPostId(Int postId);
+    List<Like> findByPostId(int postId);
 
 }
