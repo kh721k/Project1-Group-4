8435b003b625943c6efcb165ac9335880e841da8
Brittany Moliver <britt.moliver@gmail.com>
2024-07-30 14:23:43 -0400

    nav bar stuff

--- frontend/revature-p1/src/App.js
+++ frontend/revature-p1/src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        Inside App
+        <NavBar/>
+        {/* <Switch>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/registration" element={<RegistrationPage/>}></Route>
+          <Route path="/feed" element={<FeedPage/>}></Route>
+          <Route path="/users/:userId" element={<ProfilePage/>}></Route>
+          <Route path="/users/:userId/posts/:postId" element={<PostPage/>}></Route>
+          <Route path="/create-post" element={<CreatePost/>}></Route>
+        </Switch> */}
+      </BrowserRouter>
     </div>
   );
 }
