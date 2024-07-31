241d6db6221d90579a359ce0809caf0b84ce9db2
Brittany Moliver <britt.moliver@gmail.com>
2024-07-31 13:14:41 -0400

    added crossorigin to all controllerS

--- Revature-P1/src/main/java/com/revature/p1/Controllers/LikeController.java
+++ Revature-P1/src/main/java/com/revature/p1/Controllers/LikeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 public class LikeController {
 
