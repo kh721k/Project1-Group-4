2e2b19c12782c210d035a52dfe5fb91bf92f574f
Aakaash Hemdev <69800279+LemonMcSqueezy@users.noreply.github.com>
2024-07-31 15:23:03 -0400

    Update AuthContext.js

--- frontend/revature-p1/src/Context/AuthContext.js
+++ frontend/revature-p1/src/Context/AuthContext.js
@@ -1,15 +1,55 @@
-import { createContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { getCookie, removeCookie, setCookie } from 'typescript-cookie';
 
-const AuthContext = createContext({});
-
-export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
-
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
+interface contextMethods {
+    jwtToken:string | null;
+    user: string; 
+    login: (data) => void;
+    logout: () => void;
 }
 
-export default AuthContext;
+const AuthContext = createContext<contextMethods | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [user, setUser] = useState(""); 
+    const [jwtToken, setJwtToken] = useState("");
+
+    useEffect(() => {
+        const currUser = localStorage.getItem('user');
+        const currSessionToken = getCookie('Authentication');
+        if (currUser) {
+            setUser(JSON.parse(currUser));
+        }
+        if (currSessionToken) {
+            setJwtToken(currSessionToken);
+        }
+    }, []);
+
+    const login = (data: any) => { 
+        setUser(data.user);
+        setJwtToken(data.Authentication);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        setCookie('Authentication', data.Authentication, { expires: 600, path: '/' }); 
+    };
+
+    const logout = () => {
+        setUser("");
+        setJwtToken("");
+        localStorage.removeItem('user');
+        removeCookie('Authentication', { path: '/' });
+    };
+
+    return (
+        <AuthContext.Provider value={{jwtToken, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const sessionContext = useContext(AuthContext);
+    if (!sessionContext) {
+        throw new Error('AuthProvider was never used');
+    }
+    return sessionContext;
+};
